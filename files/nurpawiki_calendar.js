71abe4493782ebaa9361dffcf093e4c712adaad5
jjhellst <jjhellst@37bcf4fc-2939-0410-b639-8934a7114492>
2007-12-07 23:07:06 +0000

    Migrate js calendar to use DHTML Calendar (jscalendar) from the old one.  This simplifies code and looks nicer.

--- files/nurpawiki_calendar.js
+++ files/nurpawiki_calendar.js
@@ -1,21 +1,33 @@
 
-var cal1 = new CalendarPopup();
-
-function selectDate()
-{
-    cal1.select(this, "act_date", "yyyy-MM-dd");
-    return false;
-}
-
+// Register jscalendar selectors for cal_trigger named buttons.  Each
+// cal_trigger named button has an id of form 'button_<n>' where <n>
+// is the todo's DB id.  For each such element, there should exist a
+// 'calendar_<n>' element that contains the date to be edited.
 function nwRegisterCalendar()
 {
-    elem = document.getElementById("activation_date");
+    cals = document.getElementsByName("cal_trigger");
 
-    if(!elem)
+    re = new RegExp("^button_([0-9]+)$");
+
+    for (var i = 0; i < cals.length; i++)
     {
-        alert("JS error: couldn't find activation_date");
+        var m = re.exec(cals[i].id);
+        
+        if (m != null)
+        {
+            var todo_id = m[1];
+
+            Calendar.setup(
+                {
+                  inputField  : "calendar_"+todo_id,
+                  ifFormat    : "%Y-%m-%d",
+                  button      : m[0]
+                }
+            );
+        }
+
     }
-    elem.onclick = selectDate;
+
 }
 
 addLoadEvent(nwRegisterCalendar);
