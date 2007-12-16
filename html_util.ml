8e4d49749bbe3a330931a0a706b03fc1f1b2c779
jjhellst <jjhellst@37bcf4fc-2939-0410-b639-8934a7114492>
2007-12-16 17:21:31 +0000

    Add link to About page

--- html_util.ml
+++ html_util.ml
@@ -95,7 +95,10 @@ let navbar_html sp ~credentials ?(wiki_page_links=[]) ?(todo_list_table=[]) cont
            [td ~a:[a_class ["top_menu_right_align"]]
               ([a ~service:edit_user_page ~sp [pcdata "My Preferences"]
                   (None,credentials.user_login)] @
-                 edit_users_link @
+                 [pcdata " "] @
+                  [a ~service:about_page ~sp [pcdata "About"] ()] @
+                  [pcdata " "] @
+                  edit_users_link @
                   [pcdata " "] @
                   [disconnect_box sp "Logout"])]) []];
    div ~a:[a_id "navbar"]
